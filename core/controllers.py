6b9965ec70e77637465c3b0ed8f294fcc96c147b
wolfgang000 <wolfgangsalazarmora@hotmail.com>
2016-12-29 00:32:39 -0400

    Minimal changes

--- core/controllers.py
+++ core/controllers.py
@@ -3,3 +3,7 @@ from . import entities
 class Controller:
 	def __init__(self, repository):
 		self.__repository = repository 
+	
+	def add_task(self, task):
+		new_task = self.__repositor.add(task)
+		return new_task
