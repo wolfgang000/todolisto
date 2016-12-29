318c3e76ab1ca0d4ac87401bb0faac33183b39b3
wolfgang000 <wolfgangsalazarmora@hotmail.com>
2016-12-29 01:28:02 -0400

    Somw changes

--- core/controllers.py
+++ core/controllers.py
@@ -2,8 +2,11 @@ from . import entities
 
 class Controller:
 	def __init__(self, repository):
-		self.__repository = repository 
+		self.__repository_ = repository 
 	
 	def add_task(self, task):
 		new_task = self.__repositor.add(task)
 		return new_task
+	
+	def get_task(self, task):
+		pass
